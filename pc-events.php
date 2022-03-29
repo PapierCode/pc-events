10b2344f2fd617bd7c0a6eb1a43e20e08a37b559
papiercode <julien@papier-code.fr>
2022-03-29 10:35:56 +0200

    1.0.2 catégories, suppression métaboxe résumé

--- pc-events.php
+++ pc-events.php
@@ -4,7 +4,7 @@
 Plugin Name: [PC] Events
 Plugin URI: www.papier-code.fr
 Description: Actualités
-Version: 1.0.1
+Version: 1.0.2
 Author: Papier Codé
 */
 
--- post/fields_taxonomy.php
+++ post/fields_taxonomy.php
@@ -19,7 +19,7 @@ if ( class_exists('PC_add_field_to_tax') ) {
 
 $events_tax_content_fields_args = array(	
 	'title'     => 'Contenu',
-	'desc'		=> '<p><strong>Saisissez le titre et la description (150 mots maximum conseillés) qui s\'afficheront avant les événements associés à cette catégorie.</strong></p><p><em><strong>Remarque :</strong> si ce titre n\'est pas saisi, le nom de la catégorie est utilisé. Si l\'introduction n\'est pas saisie, la liste des événements s\'affichera directement sous le titre.</em></p>',
+	'desc'		=> '<p><strong>Saisissez le titre et l\'introduction (150 mots maximum conseillés) qui s\'afficheront avant les événements associés à cette catégorie.</strong></p><p><em><strong>Remarque :</strong> si ce titre n\'est pas saisi, le nom de la catégorie est utilisé. Si l\'introduction n\'est pas saisie, la liste des événements s\'affichera directement sous le titre.</em></p>',
 	'prefix'    => 'content',
 	'fields'    => array(
 		array(
@@ -56,7 +56,7 @@ $events_tax_content_fields = new PC_add_field_to_tax(
 
 $events_tax_img_fields_args = array(	
 	'title'     => 'Visuel',
-	'desc'		=> '<p><strong>Sélectionnez l\'image associée à cette page pour : afficher cette page sous sa forme résumée, optimiser le référencement et le partage sur les réseaux sociaux</strong>.</p><p><em><strong>Remarque :</strong> Si une image n\'est pas sélectionnée, le logo est utilisé.</em></p>',
+	'desc'		=> '<p><strong>Sélectionnez l\'image associée à cette page pour le référencement et le partage sur les réseaux sociaux</strong>.</p><p><em><strong>Remarque :</strong> Si une image n\'est pas sélectionnée, le logo est utilisé.</em></p>',
 	'prefix'    => 'visual',
 	'fields'    => array(
 		array(
@@ -78,47 +78,13 @@ $events_tax_img_fields = new PC_add_field_to_tax(
 
 /*=====  FIN Visuel  =====*/
 
-/*==============================
-=            Résumé            =
-==============================*/
-
-$events_tax_card_fields_args = array(	
-	'title'     => 'Résumé',
-	'prefix'    => 'resum',
-	'desc'		=> '<p><strong>Optimisez la description de cette page pour sa forme résumée</strong>.</p><p><em><strong>Remarque :</strong> si ce titre n\'est pas saisi, le titre du contenu est utilisé, sinon le nom de la catégorie. Si cette description n\'est pas saisie, les premiers mots de l\'introduction sont utilisés, sinon le résumé n\'affichera qu\'un visuel et un titre.</em></p>',
-	'fields'    => array(	
-		array(
-			'type'      => 'text',
-			'id'        => 'title',
-			'label'     => 'Titre',
-			'attr'      => 'class="pc-counter" data-counter-max="40"',
-			'css'		=> 'width:100%'
-		),	
-		array(
-			'type'      => 'textarea',
-			'id'        => 'desc',
-			'label'     => 'Description',
-			'attr'      => 'class="pc-counter" data-counter-max="150"',
-			'css'		=> 'width:100%'
-		)						
-	)
-);
-
-$events_tax_card_fields = new PC_add_field_to_tax(
-	EVENTS_TAX_SLUG,
-	$events_tax_card_fields_args
-);
-
-
-/*=====  FIN Résumé  =====*/
-
 /*===========================
 =            SEO            =
 ===========================*/
 
 $events_tax_seo_fields_args = array(	
 	'title'     => 'Référencement (SEO) & réseaux sociaux',
-	'desc'		=> '<p><strong>Optimisez le titre et la description pour les moteurs de recherche et les réseaux sociaux.</strong></p><p><em><strong>Remarques :</strong> si ce titre n\'est pas saisi, le titre du résumé est utilisé, sinon le titre du contenu, sinon le nom de la catégorie. Si cette description n\'est pas saisie, la description du résumé est utilisée, sinon les premiers mots de l\'introduction, sinon la description par défaut (cf. Paramètres).</em></p>',
+	'desc'		=> '<p><strong>Optimisez le titre et la description pour les moteurs de recherche et les réseaux sociaux.</strong></p><p><em><strong>Remarques :</strong> si ce titre n\'est pas saisi, le titre du contenu est utilisé, sinon le nom de la catégorie. Si cette description n\'est pas saisie, les premiers mots de l\'introduction sont utilisés, sinon la description par défaut (cf. Paramètres).</em></p>',
 	'prefix'    => 'seo',
 	'fields'    => array(
 		array(
